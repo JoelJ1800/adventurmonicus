0242561e3c32be00c43d409e8738fbdae1dd4a05
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-22 23:18:37 +0000

    player movement

---
+++ Scripts/Character/Player.gd
@@ -0,0 +1,8 @@
+class_name Player
+extends Character
+
+func _process(delta: float) -> void:
+	move_input = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	look_direction = global_position.direction_to(mouse_pos)
