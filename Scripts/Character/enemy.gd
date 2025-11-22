1a031508e0d7b766f818fb7f3b4eab1e818c87cc
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-22 23:47:46 +0000

    sprite control of enemy

--- Scripts/Character/enemy.gd
+++ Scripts/Character/enemy.gd
@@ -1,2 +1,23 @@
 class_name Enemy
 extends Character
+
+@export var target : CharacterBody2D
+@export var stop_distance : float = 28.0
+
+var target_direction : Vector2
+var target_distance : float
+
+func _process(delta: float) -> void:
+	if not target:
+		return
+	
+	#Calculate direction and distance to the target
+	target_direction = global_position.direction_to(target.global_position)
+	target_distance = global_position.distance_to(target.global_position)
+	
+	look_direction = target_direction
+	
+	if target_distance > stop_distance:
+		move_input = target_direction
+	else:
+		move_input = Vector2.ZERO
