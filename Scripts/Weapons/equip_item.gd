91780bf51593fa6632b31f032a1908c7ffb7ff12
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-23 09:05:13 +0000

    create basic weapon

---
+++ Scripts/Weapons/equip_item.gd
@@ -0,0 +1,40 @@
+class_name EquipItem
+extends Node2D
+
+@export var use_rate: float = 0.5
+var last_use_time: float
+var aim_angle: float
+var owner_character: Character
+var can_use: bool = true
+
+
+func _process(delta: float):
+	global_rotation = lerp_angle(global_rotation, aim_angle, 40 * delta)
+
+
+func set_aim_direction(aim_dir: Vector2):
+	aim_angle = aim_dir.angle()
+
+
+func _equip():
+	pass
+
+
+func _unequip():
+	pass
+
+
+func _try_use() -> bool:
+	if not can_use:
+		return false
+
+	if Time.get_unix_time_from_system() - last_use_time < use_rate:
+		return false
+
+	last_use_time = Time.get_unix_time_from_system()
+
+	return true
+
+
+func _use():
+	pass
