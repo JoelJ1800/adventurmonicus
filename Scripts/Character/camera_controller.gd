63b8d61053055f26fa3af8c9eac779f997b81787
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-24 10:24:16 +0000

    Add camera shake on damage

--- Scripts/Character/camera_controller.gd
+++ Scripts/Character/camera_controller.gd
@@ -5,20 +5,25 @@ var shake_intensity: float = 0
 
 
 func _ready():
-	pass
+	global_position = target.global_position
+	target.OnTakeDamage.connect(_on_take_damage)
 
 
 func _physics_process(delta):
-	pass
+	global_position = global_position.lerp(target.global_position, delta * 10)
 
 
 func _on_take_damage(hit_force: Vector2):
-	pass
+	shake_intensity = 4
 
 
 func _process(delta):
-	pass
+	if shake_intensity > 0:
+		shake_intensity = lerpf(shake_intensity, 0, delta * 10)
+		offset = _get_random_offset()
 
 
 func _get_random_offset() -> Vector2:
-	return Vector2.ZERO
+	var x : float = randf_range(-shake_intensity, shake_intensity)
+	var y : float = randf_range(-shake_intensity, shake_intensity)
+	return Vector2(x, y)
