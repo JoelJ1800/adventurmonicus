47f9da33d5559552b767b5c56c2b18e4c923b0c0
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-24 21:50:06 +0000

    script

--- Scripts/UI/InventorySlotUI.gd
+++ Scripts/UI/InventorySlotUI.gd
@@ -1,2 +1,16 @@
 class_name InventorySlotUI
 extends GameButton
+
+@onready var item_icon : TextureRect = $ItemIcon
+@onready var quantity_text : Label = $QuantityText
+@onready var equipped : TextureRect = $Equipped
+
+var item_slot : Inventory.ItemSlot
+var player : Player
+
+func set_item_slot (item_slot:Inventory.ItemSlot,player:Player):
+	self.item_slot = item_slot
+	self.player = player
+	var is_equipped : bool = false
+	# Change is_equipped depending on if we have this item equipped
+	equipped.visible = is_equipped
