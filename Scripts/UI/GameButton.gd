cf505431c3a825ea01cbab1a34fefc21d7185ccd
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-24 21:27:02 +0000

    connect signals to function

--- Scripts/UI/GameButton.gd
+++ Scripts/UI/GameButton.gd
@@ -2,7 +2,9 @@ class_name GameButton
 extends BaseButton
 
 func _ready():
-	pass
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	pressed.connect(_on_pressed)
 
 func _on_mouse_entered():
 	pass
