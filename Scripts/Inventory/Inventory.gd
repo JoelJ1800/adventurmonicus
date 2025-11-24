467bc4dfac395a63a6d6974612dc58feb8be53a8
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-24 21:14:28 +0000

    add item

--- Scripts/Inventory/Inventory.gd
+++ Scripts/Inventory/Inventory.gd
@@ -4,3 +4,48 @@ extends Node
 class ItemSlot:
 	var item : ItemData
 	var quantity : int
+
+signal UpdatedInventory
+signal UpdatedSlot (slot : ItemSlot)
+
+var item_slots : Array[ItemSlot]
+
+@export var size : int = 9
+@export var start_items : Dictionary[ItemData, int]
+
+func _ready():
+	pass
+
+# adds an item to the inventory
+func add_item(item: ItemData) -> bool:
+	var slot : ItemSlot = get_item_slot(item)
+	if slot and slot.quantity < item.max_stack_size:
+		slot.quantity += 1
+	else:
+		slot = get_empty_item_slot()
+		
+		if not slot:
+			return false
+		
+		slot.item = item
+		slot.quantity = 1
+	UpdatedInventory.emit()
+	UpdatedSlot.emit(slot)
+	
+	return true
+
+# revome an item to the inventory
+func remove_item(item: ItemData):
+	pass
+
+func remove_item_from_slot(slot: ItemSlot):
+	pass
+
+func get_item_slot(item: ItemData) -> ItemSlot:
+	return null
+
+func get_empty_item_slot() -> ItemSlot:
+	return null
+
+func has_item(item: ItemData) -> bool:
+	return false
