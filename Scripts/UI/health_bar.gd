5e94f3ef087d91e2ceb94bd4998ebdf1eb1f5c45
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-24 10:40:37 +0000

    create health bar percentage script only show health bar on update for 2 seconds

--- Scripts/UI/health_bar.gd
+++ Scripts/UI/health_bar.gd
@@ -1,1 +1,17 @@
 extends Panel
+
+@onready var character : Character = $".."
+@onready var fill : Control = $Fill
+@onready var max_width : float = fill.size.x
+
+func _ready ():
+	self.visible = false
+	character.OnHealthChange.connect(_update_ui)
+	_update_ui()
+
+func _update_ui():
+	var health_percent : float = float(character.cur_hp) / float(character.max_hp)
+	fill.size.x = max_width * health_percent
+	self.visible = true
+	await get_tree().create_timer(2).timeout
+	self.visible = false
