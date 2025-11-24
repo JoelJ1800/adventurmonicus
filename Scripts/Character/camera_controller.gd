8924146e99d2aef5ca53fc81a75eac3475381f42
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-24 10:10:45 +0000

    generic function setup

--- Scripts/Character/camera_controller.gd
+++ Scripts/Character/camera_controller.gd
@@ -1,4 +1,24 @@
 extends Camera2D
 
-@export var target : Character
-var shake_intensity : float = 0
+@export var target: Character
+var shake_intensity: float = 0
+
+
+func _ready():
+	pass
+
+
+func _physics_process(delta):
+	pass
+
+
+func _on_take_damage(hit_force: Vector2):
+	pass
+
+
+func _process(delta):
+	pass
+
+
+func _get_random_offset() -> Vector2:
+	return Vector2.ZERO
