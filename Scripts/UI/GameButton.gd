16f8146057a71029803f0bb8b6b55d1c2f0cbed3
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-25 17:20:00 +0000

    hover/click sounds

--- Scripts/UI/GameButton.gd
+++ Scripts/UI/GameButton.gd
@@ -1,6 +1,9 @@
 class_name GameButton
 extends BaseButton
 
+var hover_sound:AudioStream = preload("res://Audio/UI/Button_Hover.ogg")
+var click_sound:AudioStream = preload("res://Audio/UI/Button_Click.ogg")
+
 func _ready():
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
@@ -11,10 +14,12 @@ func _ready():
 func _on_mouse_entered():
 	scale.x = 1.05
 	scale.y = 1.05
+	
+	AudioManager.play(hover_sound)
 
 func _on_mouse_exited():
 	scale.x = 1.0
 	scale.y = 1.0
 
 func _on_pressed():
-	pass
+	AudioManager.play(click_sound)
