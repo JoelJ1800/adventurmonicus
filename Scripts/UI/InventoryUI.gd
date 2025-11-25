726cd208aaf67620d2df4a852db61b14f9962ebb
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-25 08:28:12 +0000

    setup functions in inventory ui script

--- Scripts/UI/InventoryUI.gd
+++ Scripts/UI/InventoryUI.gd
@@ -1,2 +1,21 @@
 class_name InventoryUI
 extends Panel
+
+@export var slots : Array[InventorySlotUI]
+@onready var inventory : Inventory = $"../../Inventory"
+@onready var player : Player = $"../.."
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _update_ui():
+	pass
+
+func _set_info_panel (slot: InventorySlotUI):
+	pass
+
+func _hide_info_panel ():
+	pass
