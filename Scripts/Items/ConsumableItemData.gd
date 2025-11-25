05786eae7fc5d190d2c20d5f11eb4d4629212db7
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-25 17:21:08 +0000

    drink sound

--- Scripts/Items/ConsumableItemData.gd
+++ Scripts/Items/ConsumableItemData.gd
@@ -2,7 +2,10 @@ class_name ConsumableItemData
 extends ItemData
 
 @export var health_gain : int = 1
+@export var consume_sound: AudioStream
 
 func _select_in_inventory (player : Player, item_slot : Inventory.ItemSlot):
 	player.heal(health_gain)
 	player.inventory.remove_item_from_slot(item_slot)
+	
+	AudioManager.play(consume_sound)
