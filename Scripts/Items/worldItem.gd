dbc40ddefafcb64fa21639a6051fd81c5d28f6c9
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-25 08:52:06 +0000

    create script variables and empty funcs

--- Scripts/Items/worldItem.gd
+++ Scripts/Items/worldItem.gd
@@ -1,2 +1,21 @@
 class_name WorldItem
 extends Area2D
+
+var item: ItemData
+
+@onready var sprite: Sprite2D = $Sprite
+
+var bob_speed: float = 10
+var bob_amount: float = 2
+
+func _process(delta: float) -> void:
+	# item bobbing
+	pass
+
+func set_item(item:ItemData):
+	pass
+
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
