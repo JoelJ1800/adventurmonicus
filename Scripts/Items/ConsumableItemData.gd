786764a3baed0c14ff7b8e7b4272d13f8529aaa1
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-25 16:53:05 +0000

    health pot heals

--- Scripts/Items/ConsumableItemData.gd
+++ Scripts/Items/ConsumableItemData.gd
@@ -2,3 +2,7 @@ class_name ConsumableItemData
 extends ItemData
 
 @export var health_gain : int = 1
+
+func _select_in_inventory (player : Player, item_slot : Inventory.ItemSlot):
+	player.heal(health_gain)
+	player.inventory.remove_item_from_slot(item_slot)
