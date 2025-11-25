6f972fcfe0f4c8e26bdfa0fd81d9da5754da8a09
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-25 16:49:29 +0000

    add select in inventory flow for player if equipped on click unequip, if no equipped, then equip

--- Scripts/Items/WeaponItemData.gd
+++ Scripts/Items/WeaponItemData.gd
@@ -1,2 +1,14 @@
 class_name WeaponItemData
 extends ItemData
+
+func _select_in_inventory (player : Player, item_slot : Inventory.ItemSlot):
+	var is_euipped: bool = player.weapons.weapon_inventory_slot == item_slot
+	
+	if is_euipped == true:
+		player.weapons.unequip_weapon()
+		player.weapons.weapon_inventory_slot = null
+	else:
+		player.weapons.equip_weapon(self)
+		player.weapons.weapon_inventory_slot = item_slot
+	
+	player.inventory.UpdatedInventory.emit()
