45023604c01f0d2e74345c665c31b94b9f5e9d36
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-26 16:36:40 +0000

    rename main to overworld add dungeon exit

--- Scripts/AutoLoads/game_manager.gd
+++ Scripts/AutoLoads/game_manager.gd
@@ -5,6 +5,8 @@ var camera : Camera2D
 
 func change_scene (scene : PackedScene,player_position : Vector2): # takes in scene to swap to, and global position to place the player on load
 	get_tree().change_scene_to_packed(scene)
+	await get_tree().process_frame
+	get_tree().root.move_child(player, get_tree().root.get_child_count() - 1)
 	
 	player.global_position = player_position
 	camera.global_position = player_position
