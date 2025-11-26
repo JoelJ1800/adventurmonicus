19929f1131a2a0c3e8f1fbe42767b4b9409dfc79
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-26 16:20:56 +0000

    add change scene to gamemanager

--- Scripts/AutoLoads/game_manager.gd
+++ Scripts/AutoLoads/game_manager.gd
@@ -1,1 +1,10 @@
 extends Node
+
+var player : Player
+var camera : Camera2D
+
+func change_scene (scene : PackedScene,player_position : Vector2):
+	get_tree().change_scene_to_packed(scene)
+	
+	player.global_position = player_position
+	camera.global_position = player_position
