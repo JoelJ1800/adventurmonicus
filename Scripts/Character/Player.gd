1fab470cfa97aa2343d679b9cdb8f5579236c2f3
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-26 16:23:18 +0000

    check the player is the current game player (remove all extra player nodes added) move the player to the Root of the tree

--- Scripts/Character/Player.gd
+++ Scripts/Character/Player.gd
@@ -3,6 +3,14 @@ extends Character
 
 @onready var inventory:Inventory = $Inventory
 
+func _ready() -> void:
+	if GameManager.player and GameManager.player != self:
+		queue_free()
+		return
+		
+	GameManager.player = self
+	reparent.call_deferred(get_tree().root)
+
 func _process(_delta: float) -> void:
 	move_input = Input.get_vector("move_left","move_right","move_up","move_down")
 	
