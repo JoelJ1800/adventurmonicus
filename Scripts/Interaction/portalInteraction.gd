f56e76cc1513af5127cef5691b4f99d3151bf1a7
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-26 16:27:24 +0000

    adjust portal script to use the GM change scene

--- Scripts/Interaction/portalInteraction.gd
+++ Scripts/Interaction/portalInteraction.gd
@@ -2,9 +2,10 @@ class_name PortalInteraction
 extends Node2D
 
 @export var scene_to_load : PackedScene
+@export var player_spawn_pos:  Vector2
 
 func _ready() -> void:
 	$Interactable.Interact.connect(_on_interact)
 
 func _on_interact(player: Player):
-	get_tree().change_scene_to_packed(scene_to_load)
+	GameManager.change_scene(scene_to_load, player_spawn_pos)
