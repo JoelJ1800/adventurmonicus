0b7fb6aee16bb8d83d5544c8ea92f70bdbfaad90
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-26 16:15:13 +0000

    add target scene to load on interacts

--- Scripts/Interaction/portalInteraction.gd
+++ Scripts/Interaction/portalInteraction.gd
@@ -1,2 +1,10 @@
 class_name PortalInteraction
 extends Node2D
+
+@export var scene_to_load : PackedScene
+
+func _ready() -> void:
+	$Interactable.Interact.connect(_on_interact)
+
+func _on_interact(player: Player):
+	get_tree().change_scene_to_packed(scene_to_load)
