fd503441af828a7152b8cd31f865694164f50818
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-26 16:21:24 +0000

    add comment

--- Scripts/AutoLoads/game_manager.gd
+++ Scripts/AutoLoads/game_manager.gd
@@ -3,7 +3,7 @@ extends Node
 var player : Player
 var camera : Camera2D
 
-func change_scene (scene : PackedScene,player_position : Vector2):
+func change_scene (scene : PackedScene,player_position : Vector2): # takes in scene to swap to, and global position to place the player on load
 	get_tree().change_scene_to_packed(scene)
 	
 	player.global_position = player_position
