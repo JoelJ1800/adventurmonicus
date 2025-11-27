1fe4457a2e1c7617773fa6c5ffe37ead6b8b7ad0
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-27 11:23:42 +0000

    creation of Base Dialogue resource creation of first npc dialogue

---
+++ Scripts/Dialogue/Dialogue.gd
@@ -0,0 +1,8 @@
+class_name Dialogue
+extends Resource
+
+@export var npc_name : String
+@export var npc_icon : Texture2D
+@export var lines : Array[String]
+@export var item_to_give : ItemData
+@export var item_to_give_quantity : int = 1
