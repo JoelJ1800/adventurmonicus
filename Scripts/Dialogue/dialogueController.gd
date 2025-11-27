49e4cfdc1e8aff7d19f3e5d5f842cd8da5b29e09
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-27 11:36:16 +0000

    add empty funcs

--- Scripts/Dialogue/dialogueController.gd
+++ Scripts/Dialogue/dialogueController.gd
@@ -1,1 +1,35 @@
+class_name DialogueController
 extends Node
+
+var current_dialogue : Dialogue
+
+@onready var dialogue_screen : Panel = $"../HUD/DialogueScreen"
+@onready var npc_name_text : Label = $"../HUD/DialogueScreen/DialoguePanel/NPCName"
+@onready var npc_icon : TextureRect = $"../HUD/DialogueScreen/DialoguePanel/NPCIcon"
+@onready var dialogue_text : Label = $"../HUD/DialogueScreen/DialoguePanel/Dialogue"
+
+@onready var player : Player = $".."
+
+var visible_chars : float
+var current_line : int
+
+func _ready():
+	# This will be called when the node enters the scene tree.
+	# We will hide the dialogue screen by default.
+	pass
+
+func set_dialogue(dialogue : Dialogue):
+	# This function will be called to start a new conversation.
+	pass
+
+func _process(delta):
+	# This function runs every frame and will be used for the text animation.
+	pass
+
+func _set_line(line : String):
+	# This will update the UI to show a specific line of dialogue.
+	pass
+
+func close_screen():
+	# This function will hide the dialogue UI and restore player control.
+	pass
