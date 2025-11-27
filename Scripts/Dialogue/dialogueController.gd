ad2dc84251db4a44f247775603438b3e47105530
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-27 11:45:47 +0000

    close the dialogue on ready, add setting of dialogue

--- Scripts/Dialogue/dialogueController.gd
+++ Scripts/Dialogue/dialogueController.gd
@@ -16,11 +16,27 @@ var current_line : int
 func _ready():
 	# This will be called when the node enters the scene tree.
 	# We will hide the dialogue screen by default.
-	pass
+	close_screen()
+
+func close_screen():
+	# This function will hide the dialogue UI and restore player control.
+	dialogue_screen.visible = false
+	current_dialogue = null
+	
+	# TODO enable player movement
 
 func set_dialogue(dialogue : Dialogue):
 	# This function will be called to start a new conversation.
-	pass
+	current_dialogue = dialogue
+	
+	dialogue_screen.visible = true
+	npc_name_text.text = dialogue.npc_name
+	npc_icon.texture = dialogue.npc_icon
+	
+	current_line = -1
+	_set_line(dialogue.lines[0])
+	
+	# TODO disable player movement
 
 func _process(delta):
 	# This function runs every frame and will be used for the text animation.
@@ -28,8 +44,4 @@ func _process(delta):
 
 func _set_line(line : String):
 	# This will update the UI to show a specific line of dialogue.
-	pass
-
-func close_screen():
-	# This function will hide the dialogue UI and restore player control.
 	pass
