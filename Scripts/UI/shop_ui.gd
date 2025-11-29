3545cfc5064b5219d36de1a9c233dd76d4855ce6
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-11-29 11:29:01 +0000

    populate player_items and shop_items arrays.

--- Scripts/UI/shop_ui.gd
+++ Scripts/UI/shop_ui.gd
@@ -10,3 +10,16 @@ extends Panel
 var shop : Shop
 var player_items : Array[ShopItemUI]
 var shop_items : Array[ShopItemUI]
+
+func _ready():
+	for child in player_items_container.get_children():
+		if child is not ShopItemUI:
+			continue
+		
+		player_items.append(child)
+	
+	for child in shop_items_container.get_children():
+		if child is not ShopItemUI:
+			continue
+		
+		shop_items.append(child)
