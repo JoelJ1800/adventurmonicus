729a3e7fffc83442de2dc1ec2d6efc823b29d55b
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-12-02 16:30:12 +0000

    start creating animation movement

--- Scenes/NewOverwerld/SwordsmanPlayer.gd
+++ Scenes/NewOverwerld/SwordsmanPlayer.gd
@@ -3,14 +3,22 @@ extends CharacterBody2D
 @onready var move_state_machine:  AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/MoveStateMachine/playback")
 @onready var sword_state_machine:  AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/SwordStateMachine/playback")
 
+
 #movement
 var direction: Vector2
 var last_direction: Vector2
-@export var speed: int = 5
-@export var sprint_speed: int = 5
+@export var speed: int = 50
+@export var sprint_speed: int = 100
 @export var tool_offset:= 20
 var can_move: bool = true
 var building:= false
+const WALK_SPEED_THRESHOLD = 80.0
+const RUN_SPEED_THRESHOLD = 160.0
+
+var move_state =  get_move_state()
+
+
+
 
 func _physics_process(_delta: float) -> void:
 	if can_move:
@@ -23,7 +31,23 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func get_input():
-	pass
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+	if Input.is_action_just_pressed("attack"):
+		#get_move_state()
+		print(move_state)
+		#sword_state_machine.travel(state_names[current_tool])
+		#$AnimationTree.set("parameters/ToolOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+		#can_move = false
 
 func set_animation():
 	pass
+
+func get_move_state() -> String:
+	var speed := velocity.length()
+	if speed < 5:
+		return "idle"
+	elif speed < WALK_SPEED_THRESHOLD:
+		return "walking"
+	else:
+		return "running"
