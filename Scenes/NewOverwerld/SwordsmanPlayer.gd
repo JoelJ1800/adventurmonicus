56df5bbfc1dc808a8577fed252eb39946d1f95ff
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-12-02 16:54:08 +0000

    update structure

--- Scenes/NewOverwerld/SwordsmanPlayer.gd
+++ Scenes/NewOverwerld/SwordsmanPlayer.gd
@@ -15,7 +15,7 @@ var building:= false
 const WALK_SPEED_THRESHOLD = 80.0
 const RUN_SPEED_THRESHOLD = 160.0
 
-var move_state =  get_move_state()
+var move_state 
 
 
 
@@ -23,31 +23,48 @@ var move_state =  get_move_state()
 func _physics_process(_delta: float) -> void:
 	if can_move:
 		get_input()
-		set_animation()
+		#set_animation()
+	
 	if Input.is_action_pressed("sprint"):
 		velocity = direction * sprint_speed * int(can_move)
 	else:
 		velocity = direction * speed * int(can_move)
 	move_and_slide()
+	print(velocity)
 
 func get_input():
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
-	if Input.is_action_just_pressed("attack"):
+	#if Input.is_action_just_pressed("attack"):
 		#get_move_state()
-		print(move_state)
+		#print(move_state)
 		#sword_state_machine.travel(state_names[current_tool])
 		#$AnimationTree.set("parameters/ToolOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 		#can_move = false
 
 func set_animation():
-	pass
-
-func get_move_state() -> String:
-	var speed := velocity.length()
-	if speed < 5:
-		return "idle"
-	elif speed < WALK_SPEED_THRESHOLD:
-		return "walking"
+	
+	if direction:
+		move_state_machine.travel('Walk')
+		var direction_animation: Vector2 = Vector2(round(direction.x), round(direction.y))
+		$AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", direction_animation)
+		$AnimationTree.set("parameters/MoveStateMachine/Idle/blend_position", direction_animation)
+	#elif direction and get_move_state() == "running":
+		#move_state_machine.travel('Running')
+		#var direction_animation: Vector2 = Vector2(round(direction.x), round(direction.y))
+		#$AnimationTree.set("parameters/MoveStateMachine/Running/blend_position", direction_animation)
+		#$AnimationTree.set("parameters/MoveStateMachine/Idle/blend_position", direction_animation)
 	else:
-		return "running"
+		move_state_machine.travel('Idle')
+
+func get_move_state():
+	var current_speed := velocity.length()
+	if current_speed < 5:
+		move_state = "idle"
+	elif current_speed < WALK_SPEED_THRESHOLD:
+		move_state = "walking"
+	else:
+		move_state = "running"
+
+func _on_animation_tree_animation_finished(_anim_name: StringName) -> void:
+	pass
