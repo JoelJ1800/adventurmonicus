b7c7a074d3f8f6382d50b9ec69d109b338b07aa3
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-12-02 17:09:51 +0000

    Add conditional sprint animation

--- Scenes/NewOverwerld/SwordsmanPlayer.gd
+++ Scenes/NewOverwerld/SwordsmanPlayer.gd
@@ -28,30 +28,29 @@ func _physics_process(_delta: float) -> void:
 		velocity = direction * sprint_speed * int(can_move)
 	else:
 		velocity = direction * speed * int(can_move)
+	get_move_state()
 	move_and_slide()
 
 func get_input():
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-
-	#if Input.is_action_just_pressed("attack"):
-		#get_move_state()
-		#print(move_state)
+	if Input.is_action_just_pressed("attack"):
+		print(move_state)
 		#sword_state_machine.travel(state_names[current_tool])
 		#$AnimationTree.set("parameters/ToolOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 		#can_move = false
 
 func set_animation():
 	
-	if direction.length() > 0:
+	if direction.length() > 0 and move_state == "walking":
 		move_state_machine.travel('Walk')
 		var direction_animation: Vector2 = Vector2(round(direction.x), round(direction.y))
 		$AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", direction_animation)
 		$AnimationTree.set("parameters/MoveStateMachine/Idle/blend_position", direction_animation)
-	#elif direction and get_move_state() == "running":
-		#move_state_machine.travel('Running')
-		#var direction_animation: Vector2 = Vector2(round(direction.x), round(direction.y))
-		#$AnimationTree.set("parameters/MoveStateMachine/Running/blend_position", direction_animation)
-		#$AnimationTree.set("parameters/MoveStateMachine/Idle/blend_position", direction_animation)
+	elif direction and move_state == "running":
+		move_state_machine.travel('Running')
+		var direction_animation: Vector2 = Vector2(round(direction.x), round(direction.y))
+		$AnimationTree.set("parameters/MoveStateMachine/Running/blend_position", direction_animation)
+		$AnimationTree.set("parameters/MoveStateMachine/Idle/blend_position", direction_animation)
 	else:
 		move_state_machine.travel('Idle')
 
