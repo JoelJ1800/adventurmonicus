3efb6d15e8e126e258b4949e4bdf76b85745e0c2
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-12-10 13:43:11 +0000

    attach script to all entrances add Room Script Variables

--- Scenes/NewDungeon/Room.gd
+++ Scenes/NewDungeon/Room.gd
@@ -8,3 +8,12 @@ enum Direction
 	EAST,
 	WEST
 }
+
+@export var doors_always_open : bool = false
+
+@onready var entrance_north : RoomEntrance = $Entrance_North
+@onready var entrance_south : RoomEntrance = $Entrance_South
+@onready var entrance_east : RoomEntrance = $Entrance_East
+@onready var entrance_west : RoomEntrance = $Entrance_West
+
+var enemies_in_room : int
