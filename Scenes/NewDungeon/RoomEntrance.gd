2f3d4070a9f2f2c48b3279940c61a31956ac48ad
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-12-10 13:24:44 +0000

    comment and format

--- Scenes/NewDungeon/RoomEntrance.gd
+++ Scenes/NewDungeon/RoomEntrance.gd
@@ -1,29 +1,36 @@
 class_name RoomEntrance
 extends Node2D
 
-@export var direction : Room.Direction = Room.Direction.NORTH
+@export var direction: Room.Direction = Room.Direction.NORTH
 @onready var door = $DoorGate
 @onready var door_anim = $DoorGate/DoorGate
 @onready var door_closed_collider = $DoorGate/col_Door_Closed
 @onready var player_spawn = $PlayerSpawn
 @onready var exit_trigger: Area2D = $PlayerExit
-var neighbor : Room
+var neighbor: Room
+
 
 func _ready() -> void:
 	exit_trigger.body_entered.connect(_on_body_entered_exit_trigger)
 
-func set_neightbour (neighbour_room : Room):
+
+func set_neightbour(neighbour_room: Room):
 	pass
 
-func toggle_barrier (toggle : bool):
-	pass
-func open_door ():
-	door_anim.play("DoorOpen")
-	door_closed_collider.disabled = true
 
-func close_door ():
-	door_anim.play("DoorClose")
-	door_closed_collider.disabled = false
+func toggle_barrier(toggle: bool):
+	pass
+
+
+func open_door():
+	door_anim.play("DoorOpen") # plays open animation
+	door_closed_collider.disabled = true # removes collider over the doorway
+
+
+func close_door():
+	door_anim.play("DoorClose") # plays close animation
+	door_closed_collider.disabled = false # adds collider over the doorway, default collider is on
+
 
 func _on_body_entered_exit_trigger(body: Node2D) -> void:
 	pass
