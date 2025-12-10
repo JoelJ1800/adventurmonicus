51268daae8cf1b4f9e69191592bbd8fa3cf5dd3b
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-12-10 13:22:40 +0000

    create open and close door methods

--- Scenes/NewDungeon/RoomEntrance.gd
+++ Scenes/NewDungeon/RoomEntrance.gd
@@ -2,10 +2,28 @@ class_name RoomEntrance
 extends Node2D
 
 @export var direction : Room.Direction = Room.Direction.NORTH
-@onready var doorAnim = $DoorGate/DoorGate
 @onready var door = $DoorGate
 @onready var door_anim = $DoorGate/DoorGate
 @onready var door_closed_collider = $DoorGate/col_Door_Closed
 @onready var player_spawn = $PlayerSpawn
 @onready var exit_trigger: Area2D = $PlayerExit
 var neighbor : Room
+
+func _ready() -> void:
+	exit_trigger.body_entered.connect(_on_body_entered_exit_trigger)
+
+func set_neightbour (neighbour_room : Room):
+	pass
+
+func toggle_barrier (toggle : bool):
+	pass
+func open_door ():
+	door_anim.play("DoorOpen")
+	door_closed_collider.disabled = true
+
+func close_door ():
+	door_anim.play("DoorClose")
+	door_closed_collider.disabled = false
+
+func _on_body_entered_exit_trigger(body: Node2D) -> void:
+	pass
