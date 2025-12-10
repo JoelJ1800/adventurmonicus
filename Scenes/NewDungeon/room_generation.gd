b4457c3d2d8a548809fcac7ce6e982f91b925ace
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-12-10 10:11:18 +0000

    add generation and print array output

--- Scenes/NewDungeon/room_generation.gd
+++ Scenes/NewDungeon/room_generation.gd
@@ -1,8 +1,8 @@
 class_name RoomGeneration
 extends Node
 
-@export var map_size: int = 7
-@export var rooms_to_generate: int = 12
+@export var map_size: int = 10
+@export var rooms_to_generate: int = 30
 var room_count: int = 0
 var map: Array[bool]
 var rooms: Array[Room]
@@ -18,6 +18,13 @@ var room_scene: PackedScene = preload("res://Scenes/NewDungeon/circular_room.tsc
 
 func _ready():
 	_generate() # on load the scene run the generation function
+	
+		# generate preview
+	for x in range(map_size):
+		var line:String
+		for y in range(map_size):
+			line += "#" if _get_map(x,y) else "O"
+		print(line)
 
 
 func _generate():
@@ -39,6 +46,24 @@ func _check_room(x: int, y: int, desired_direction: Vector2, is_first_room: bool
 	if _get_map(x, y):
 		return # stop generating if attempting to spawn on an existing room
 	room_count += 1
+	_set_map(x,y,true)
+	
+	# 80% chance to follow the desired direction 20% chance to choose another
+	var go_north: bool = randf() > (0.4 if desired_direction == Vector2.UP else 0.6)
+	var go_south: bool = randf() > (0.4 if desired_direction == Vector2.DOWN else 0.6)
+	var go_east: bool = randf() > (0.4 if desired_direction == Vector2.RIGHT else 0.6)
+	var go_west: bool = randf() > (0.4 if desired_direction == Vector2.LEFT else 0.6)
+	
+	# go compassDir if its the first room, else go desiredDir
+	if go_north or is_first_room:
+		_check_room(x, y-1, Vector2.UP if is_first_room else desired_direction)
+	if go_south or is_first_room:
+		_check_room(x, y+1, Vector2.DOWN if is_first_room else desired_direction) 
+	if go_east or is_first_room:
+		_check_room(x + 1, y, Vector2.RIGHT if is_first_room else desired_direction) 
+	if go_west or is_first_room:
+		_check_room(x - 1, y, Vector2.LEFT if is_first_room else desired_direction) 
+	
 
 
 func _get_map(x: int, y: int) -> bool: # helper function to check if there is a room on the checked slot
