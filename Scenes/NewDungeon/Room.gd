be8bb7285d436afc963d55167ed8e3b202cb5e30
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-12-11 16:03:18 +0000

    formatting

--- Scenes/NewDungeon/Room.gd
+++ Scenes/NewDungeon/Room.gd
@@ -8,9 +8,9 @@ enum Direction {
 	WEST,
 }
 
-@export var enemy_scenes : Array[PackedScene] = []
-@export var min_distance_from_player :float = 200.0
-@export var max_enemies : int = 4
+@export var enemy_scenes: Array[PackedScene] = []
+@export var min_distance_from_player: float = 200.0
+@export var max_enemies: int = 4
 @export var doors_always_open: bool = false
 
 @onready var entrance_north: RoomEntrance = $Overworld/EntranceNorth
@@ -30,7 +30,7 @@ func initialize():
 	pass
 
 
-func set_neighbour(neighbour_direction : Direction, neighbour_room : Room):
+func set_neighbour(neighbour_direction: Direction, neighbour_room: Room):
 	if neighbour_direction == Direction.NORTH:
 		entrance_north.set_neighbour(neighbour_room)
 	elif neighbour_direction == Direction.SOUTH:
@@ -53,15 +53,14 @@ func player_enter(entry_direction: Direction, player: CharacterBody2D, first_roo
 		player.global_position = entrance_west.player_spawn.global_position
 
 	if first_room:
-		player.global_position = global_position + Vector2(400,400)
+		player.global_position = global_position + Vector2(400, 400)
 
 	if enemies_in_room > 0 and not doors_always_open:
 		close_doors()
 	else:
 		open_doors()
-	
+
 	spawn_enemies.call_deferred(player.global_position)
-	
 
 
 func _on_defeat_enemy(enemy):
@@ -85,7 +84,7 @@ func close_doors():
 func spawn_enemies(current_spawn):
 	if enemy_scenes.is_empty():
 		return
-	
+
 	if doors_always_open == true:
 		return
 
@@ -95,7 +94,6 @@ func spawn_enemies(current_spawn):
 	for spawn in enemy_spawns.get_children():
 		if spawn.global_position.distance_to(current_spawn) >= min_distance_from_player:
 			possible_spawns.append(spawn)
-	
 	print(possible_spawns)
 
 	if possible_spawns.is_empty():
@@ -111,4 +109,4 @@ func spawn_enemies(current_spawn):
 		var enemy = enemy_scene.instantiate()
 		enemy.global_position = spawn_point.position
 		add_child(enemy)
-		print("enemy spawned at " +str(spawn_point) + " " + str(spawn_point.position))
+		print("enemy spawned at " + str(spawn_point) + " " + str(spawn_point.position))
