fc5c9384ce0944939d6681fb1aa0bdc9b630e433
Joel J <87704817+JoelJ1800@users.noreply.github.com>
2025-12-11 16:02:58 +0000

    create enemy generation script

--- Scenes/NewDungeon/Room.gd
+++ Scenes/NewDungeon/Room.gd
@@ -8,12 +8,16 @@ enum Direction {
 	WEST,
 }
 
+@export var enemy_scenes : Array[PackedScene] = []
+@export var min_distance_from_player :float = 200.0
+@export var max_enemies : int = 4
 @export var doors_always_open: bool = false
 
 @onready var entrance_north: RoomEntrance = $Overworld/EntranceNorth
 @onready var entrance_south: RoomEntrance = $Overworld/EntranceSouth
 @onready var entrance_east: RoomEntrance = $Overworld/EntranceEast
 @onready var entrance_west: RoomEntrance = $Overworld/EntranceWest
+@onready var enemy_spawns = $EnemySpawns
 
 var enemies_in_room: int
 
@@ -55,6 +59,9 @@ func player_enter(entry_direction: Direction, player: CharacterBody2D, first_roo
 		close_doors()
 	else:
 		open_doors()
+	
+	spawn_enemies.call_deferred(player.global_position)
+	
 
 
 func _on_defeat_enemy(enemy):
@@ -73,3 +80,35 @@ func close_doors():
 	entrance_south.close_door.call_deferred()
 	entrance_east.close_door.call_deferred()
 	entrance_west.close_door.call_deferred()
+
+
+func spawn_enemies(current_spawn):
+	if enemy_scenes.is_empty():
+		return
+	
+	if doors_always_open == true:
+		return
+
+	var possible_spawns = []
+
+	# Filter spawn points too close to the player spawn
+	for spawn in enemy_spawns.get_children():
+		if spawn.global_position.distance_to(current_spawn) >= min_distance_from_player:
+			possible_spawns.append(spawn)
+	
+	print(possible_spawns)
+
+	if possible_spawns.is_empty():
+		return
+
+	possible_spawns.shuffle()
+
+	var num_to_spawn = min(max_enemies, possible_spawns.size())
+
+	for i in num_to_spawn:
+		var spawn_point = possible_spawns[i]
+		var enemy_scene = enemy_scenes.pick_random()
+		var enemy = enemy_scene.instantiate()
+		enemy.global_position = spawn_point.position
+		add_child(enemy)
+		print("enemy spawned at " +str(spawn_point) + " " + str(spawn_point.position))
